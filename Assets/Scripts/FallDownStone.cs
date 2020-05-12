1d72866f5df4c286138b08a7419ff859ce9c396d
Dmitry ZiP <zexsty55@gmail.com>
2020-05-12 19:09:03 +0100

    Добавлена кнопка
    Камень и анимации

--- Assets/Scripts/FallDownStone.cs
+++ Assets/Scripts/FallDownStone.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class FallDownStone : MonoBehaviour
 {
-
-    [SerializeField]
-    private float fallSpeed;
-
     void Update()
     {
         if (transform.position.y < 2.0f)// Уничтожение обьекта бомы за границами экрана
             Destroy(gameObject, 5f);
-
-        transform.position -= new Vector3(0, fallSpeed * Time.deltaTime, 0);
     }
 }
--- Assets/Scripts/SpawnObject/SpawnShield.cs
+++ Assets/Scripts/SpawnObject/SpawnShield.cs
@@ -14,7 +14,7 @@ public class SpawnShield : MonoBehaviour
     {
         while (!Player.lose)
         {
-            yield return new WaitForSeconds(Random.Range(5.0f, 6.0f)); // Задержка при создани и  Рандомный спавн обьекта
+            yield return new WaitForSeconds(Random.Range(30f, 120f)); // Задержка при создани и  Рандомный спавн обьекта
             Instantiate(shield, new Vector2(Random.Range(-2.85f, 2.85f), 5.521f), Quaternion.identity); //Рандомное расположение падения обьекта
             
         }
