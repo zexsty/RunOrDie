31893dc3a846793a37ecf8dbdf2d79a235a7e2c3
Dmitry ZiP <zexsty55@gmail.com>
2020-06-19 08:49:35 +0100

    изменён главный фон игры, анимированы звезды и планеты

---
+++ Assets/Scripts/RotateAroundPlanet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPlanet : MonoBehaviour
+{
+    public Transform rotateObj;
+    public Transform aroundObj;
+    public float rotSpeed = 5f;
+    void Update()
+    {
+        Rotator();
+    }
+    void Rotator()
+    {
+        rotateObj.RotateAround(aroundObj.position, new Vector3(0, 0, -1), rotSpeed);
+    }
+}
---
+++ Assets/Scripts/SpawnObject/StarsMainFon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsMainFon : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _stars;
+    private GameObject _insStars;
+
+    void Start()
+    {
+        StartCoroutine(SpawnStarFon());
+    }
+
+    private IEnumerator SpawnStarFon()
+    {
+        while (!PlayerController.lose)
+        {
+            yield return new WaitForSeconds(Random.Range(1f, 3f));
+          _insStars =  Instantiate(_stars[Random.Range(0, 2)], new Vector2(Random.Range(-2.85f, 2.85f), Random.Range(0.1f, 5.0f)), Quaternion.identity);
+            GameObject.Destroy(_insStars, 5f);
+        }
+        
+    }
+}
+
